7606985fbbcbd5c63f61c7ddd741677d2ba28b28
jenberu <jemberu0970@gmail.com>
2025-03-11 14:21:25 +0300

    update app configuration; add custom fonts and enhance Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubic-Regular", "sans-serif"],
+        "rubik-bold": ["Rubic-Bold", "sans-serif"],
+        "rubic-extraBold": ["Rubic-ExtraBold", "sans-serif"],
+        "rubic-medium": ["Rubic-Medium", "sans-serif"],
+        "rubic-light": ["Rubic-Light", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          200: "#0061FF1A",
+          300: "#0061FF",
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#0000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191d31",
+        },
+        danger: "#F75555",
+      },
+    },
   },
   plugins: [],
-}
+};
